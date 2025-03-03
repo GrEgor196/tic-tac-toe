41a699d829b02dc5ba745ea7a2d49aa394faf3bc
GrEgor196 <E.R.Grishin@urfu.me>
2025-03-03 12:29:58 +0500

    Task done

--- index.js
+++ index.js
@@ -3,12 +3,22 @@ const ZERO = 'O';
 const EMPTY = ' ';
 
 const container = document.getElementById('fieldWrapper');
+let board;
+let player;
+let gameOver;
+let dimension = 3;
 
 startGame();
 addResetListener();
 
 function startGame () {
-    renderGrid(3);
+    board = [];
+    for (let i = 0; i < dimension; i++) {
+        board.push(Array(dimension).fill(EMPTY));
+    }
+    player = CROSS;
+    gameOver = false;
+    renderGrid(dimension);
 }
 
 function renderGrid (dimension) {
@@ -27,13 +37,67 @@ function renderGrid (dimension) {
 }
 
 function cellClickHandler (row, col) {
-    // Пиши код тут
     console.log(`Clicked on cell: ${row}, ${col}`);
+    if (gameOver || board[row][col] !== EMPTY) {
+        return;
+    }
+    board[row][col] = player;
+    renderSymbolInCell(player, row, col)
 
+    const winningCells = checkWinner(player);
+    if (winningCells) {
+        gameOver = true;
+        highlightWinningCells(winningCells);
+        alert(`Победил ${player}!`);
+        return;
+    }
 
-    /* Пользоваться методом для размещения символа в клетке так:
-        renderSymbolInCell(ZERO, row, col);
-     */
+    if (board.flat().every(cell => cell !== EMPTY)) {
+        alert("Победила дружба!");
+        gameOver = true;
+        return;
+    }
+
+    player = player === CROSS ? ZERO : CROSS;
+    if (player === ZERO) AIMove();
+}
+
+function checkWinner(player) {
+
+    for (let i = 0; i < dimension; i++) {
+        if (board[i].every(cell => cell === player)) {
+            return board[i].map((_, j) => [i, j]);
+        }
+        if (board.map(row => row[i]).every(cell => cell === player)) {
+            return board.map((_, j) => [j, i]);
+        }
+    }
+
+    if (board.map((row, i) => row[i]).every(cell => cell === player)) {
+        return board.map((_, i) => [i, i]);
+    }
+    if (board.map((row, i) => row[dimension - i - 1]).every(cell => cell === player)) {
+        return board.map((_, i) => [i, dimension - i - 1]);
+    }
+
+    return null;
+}
+
+function AIMove () {
+    let cellsToStep = [];
+    for (let i = 0; i < dimension; i++) {
+        for (let j = 0; j < dimension; j++) {
+            if (board[i][j] === EMPTY) cellsToStep.push([i, j]);
+        }
+    }
+    if (cellsToStep.length === 0) return;
+
+    let [row, col] = cellsToStep[Math.floor(Math.random() * cellsToStep.length)];
+    cellClickHandler(row, col);
+}
+
+function highlightWinningCells(cells) {
+    cells.forEach(([row, col]) => renderSymbolInCell(board[row][col], row, col, 'red'));
 }
 
 function renderSymbolInCell (symbol, row, col, color = '#333') {
@@ -55,6 +119,7 @@ function addResetListener () {
 
 function resetClickHandler () {
     console.log('reset!');
+    startGame();
 }
 
 
